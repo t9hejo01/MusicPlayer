99919095b212b80959a775ccb8962eb01524e300
Joona <t9hejo01@students.oamk.fi>
2021-04-27 14:26:23 +0300

    Create MusicPlayer app with offline support

---
+++ app/src/main/java/music/palyer/MainActivity.java
@@ -0,0 +1,34 @@
+package music.palyer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "offline";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOffline = findViewById(R.id.btnOffline);
+        btnOffline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OfflineActivity.class));
+            }
+        });
+
+        Button btnOnline = findViewById(R.id.btnOnline);
+        btnOnline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SearchOnlineActivity.class));
+            }
+        });
+    }
+}
