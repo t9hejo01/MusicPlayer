178e36eccf214aec486157dda1794955c4433362
Joona <t9hejo01@students.oamk.fi>
2021-04-28 14:32:26 +0300

    Modificate xml - files

---
+++ app/src/main/java/music/player/OfflineActivityTry.java
@@ -0,0 +1,14 @@
+package music.player;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OfflineActivityTry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offline_try);
+    }
+}
