5aad642ab24413179dbae77edfb171ce50dbee3f
Joona <t9hejo01@students.oamk.fi>
2021-04-28 15:22:24 +0300

    Modificate code

--- app/src/main/java/music/player/OfflineActivityTry.java
+++ app/src/main/java/music/player/OfflineActivityTry.java
@@ -1,14 +1,79 @@
 package music.player;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class OfflineActivityTry extends AppCompatActivity {
+import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
+public class OfflineActivityTry extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static final String TAG = "offline";
+    ConstraintLayout constraintLayout;
+    mediaAdapterSwipe adapterSwipe;
+    ListView listView;
+    RecyclerView rvTrackList;
+    MediaAdapter mediaAdapter;
+    ArrayList<Audio> audioList;
+    TextView tvTotalSongs;
+    StorageReference storageReference;
+    ProgressDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline_try);
+        if (getIntent().getStringExtra("uid") != null) {
+            StorageUtil.uid = getIntent().getStringExtra("uid");
+            Toast.makeText(getApplicationContext(), StorageUtil.uid, Toast.LENGTH_SHORT).show();
+        }
+
+        if (getIntent().getStringExtra("username") != null) {
+            StorageUtil.username = getIntent().getStringExtra("username");
+        }
+
+        dialog = new ProgressDialog(this);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        View header = navigationView.getHeaderView(0);
+        TextView name = header.findViewById(R.id.username);
+        name.setText(StorageUtil.username);
     }
 }
